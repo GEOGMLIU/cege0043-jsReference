2b6c66e6a4a2602f74dd858dae229b8c1de80cd9
Maozhu Liu <uczlm97@ucl.ac.uk>
2019-03-14 06:36:37 -0400

    using modules to test the server's result

--- studentServer.js
+++ studentServer.js
@@ -10,27 +10,40 @@ var http = require('http');
 var httpServer = http.createServer(app);
 httpServer.listen(4480);
 var bodyParser = require('body-parser');
+
 app.use(bodyParser.urlencoded({
-extended: true
+	extended: true
 }));
 app.use(bodyParser.json());
+
+var fs = require('fs');
+var pg = require('pg');
+var configtext =""+fs.readFileSync("/home/studentuser/certs/postGISConnection.js");
+// now convert the configruation file into the correct format -i.e. a name/value pair array
+var configarray = configtext.split(",");
+var config = {};
+for (var i = 0; i < configarray.length; i++) {
+	var split = configarray[i].split(':');
+	config[split[0].trim()] = split[1].trim();
+}
+var pool = new pg.Pool(config);
 /*app.get('/',function (req,res) {
 	res.send("hello world from the HTTP server");
-});*/
-/*app.get('/test.html', function (req, res) {
+});
+app.get('/test.html', function (req, res) {
 	// run some server-side code
 	console.log('test.html requested');
 	// note that __dirname gives the path to the studentServer.js file
 	res.sendFile(__dirname + '/test.html');
-});*/
-/*app.get('/:fileName', function (req, res) {
+});
+app.get('/:fileName', function (req, res) {
 	// run some server-side code
 	var fileName = req.params.fileName;
 	console.log(fileName + ' requested');
 	// note that __dirname gives the path to the studentServer.js file
 	res.sendFile(__dirname + '/'+ fileName);
-});*/
-/*app.use(function(req, res, next) {
+});
+app.use(function(req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "X-Requested-With");
 	next();
@@ -43,11 +56,60 @@ app.use(function (req, res, next) {
 	next();
 });*/
 
-
+app.get('/postgistest', function (req,res) {
+	pool.connect(function(err,client,done) {
+		if(err){
+			console.log("not able to get connection "+ err);
+			res.status(400).send(err);
+		}
+		client.query('SELECT name FROM london_poi' ,function(err,result) {
+			done();
+			if(err){
+				console.log(err);
+				res.status(400).send(err);
+			}
+			res.status(200).send(result.rows);
+		});
+	});
+});
+app.post('/reflectData',function(req,res){
+	// note that we are using POST here as we are uploading data
+	// so the parameters form part of the BODY of the request rather
+	//than the RESTful API
+	console.dir(req.body);
+	// for now, just echo the request back to the client
+	res.send(req.body);
+});
 app.use(function(req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "X-Requested-With");
 	next();
+});
+
+app.post('/uploadData',function(req,res){
+// note that we are using POST here as we are uploading data
+// so the parameters form part of the BODY of the request rather than the RESTful API
+	console.dir(req.body);
+	pool.connect(function(err,client,done) {
+		if(err){
+			console.log("not able to get connection "+ err);
+			res.status(400).send(err);
+		}
+		var name = req.body.name;
+		var surname = req.body.surname;
+		var module = req.body.module;
+		var portnum = req.body.port_id;
+		var querystring = "INSERT into formdata (name,surname,module, port_id)values ($1,$2,$3,$4) ";
+		console.log(querystring);
+		client.query( querystring,[name,surname,module,portnum],function(err,result) {
+			done();
+			if(err){
+				console.log(err);
+				res.status(400).send(err);
+			}
+			res.status(200).send("row inserted");
+		});
+	});
 });
 
 
